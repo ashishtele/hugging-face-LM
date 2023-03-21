d0ece29e0ec19368aac8785b2413a9f32a6c4135
Ashish Tele <sale.ashish.tele@gmail.com>
2023-03-21 01:01:30 +0000

    Main code added

---
+++ app.py
@@ -0,0 +1,14 @@
+from transformers import pipeline
+import gradio as gr
+
+model = pipeline("summarization")
+
+def predict(prompt):
+    summary = model(prompt)[0]["summary_text"]
+    return summary
+
+with gr.Blocks() as demo:
+    textbox = gr.Textbox(placeholder="Enter text block to summarize", lines=4)
+    gr.Interface(fn=predict, inputs=textbox, outputs="text")
+
+demo.launch(share=True)
